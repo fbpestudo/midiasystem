42becae8986f8ceaa6c04d034177e444c0715bb4
Fabio Eduardo Bernardo Barros <fabiobarros@saovicente.local>
2018-07-31 14:57:01 -0300

    Trabalhando na caixa ferramentas

--- js/main.js
+++ js/main.js
@@ -1,102 +1,122 @@
-function mudarTitulo(){
+function mudarTitulo() {
     var valor = document.getElementById("id5").value;
     var p5 = document.getElementById("titulo");
     p5.innerHTML = valor;
 }
-function mudarSub(){
+function mudarSub() {
     var valor = document.getElementById("id6").value;
     var p6 = document.getElementById("subtitulo");
     p6.innerHTML = valor;
 }
-function mudarData(){
+function mudarData() {
     var valor = document.getElementById("id1").value;
     var p1 = document.getElementById("data");
-    p1.innerHTML = "Data: "+valor;
+    p1.innerHTML = "Data: " + valor;
 }
 
-function mudarHora(){
+function mudarHora() {
     var valor = document.getElementById("id2").value;
     var p2 = document.getElementById("hora");
-    p2.innerHTML = "Hora: "+valor;
+    p2.innerHTML = "Hora: " + valor;
 }
 
-function mudarTelefone(){
+function mudarTelefone() {
     var valor = document.getElementById("id3").value;
     var p3 = document.getElementById("tel");
-    p3.innerHTML = "Telefone: "+valor;
+    p3.innerHTML = "Telefone: " + valor;
 }
 
-function mudarLocal(){
+function mudarLocal() {
     var valor = document.getElementById("id4").value;
     var p4 = document.getElementById("local");
-    p4.innerHTML = "Local: " +valor;
+    p4.innerHTML = "Local: " + valor;
 }
 
-function mudarOpc(){
-    var valor= document.getElementById("id7").value;
-    var p7= document.getElementById("txtopc");
-    p7.innerHTML =valor;
+function mudarOpc() {
+    var valor = document.getElementById("id7").value;
+    var p7 = document.getElementById("txtopc");
+    p7.innerHTML = valor;
 }
 
-function mudarTipo(x){
-    var valor= document.getElementById("tipo").value;
+function mudarTipo(x) {
+    var valor = document.getElementById("tipo").value;
     var c = document.getElementById(x);
-   switch (valor){
-       case "Arial":
-    //    c.style.color = valor;
-       c.style.fontFamily = "Arial,sans-serif";
-    //    $("#subtitulo").css("font-family", "Arial,sans-serif");
-       break;
-       case "Helvetica":
-       $("#subtitulo").css("font-family", "Helvetica");
-       break;
-       case "Times":
-    //    $("#subtitulo").css("font-family", "Times, serif");
-       c.style.fontFamily = "Times, serif";
-       break;
-       case "Georgia":
-       $("#subtitulo").css("font-family", "Georgia");
-       break;
-   }
+    switch (valor) {
+        case "Arial":
+            //    c.style.color = valor;
+            c.style.fontFamily = "Arial,sans-serif";
+            //    $("#subtitulo").css("font-family", "Arial,sans-serif");
+            break;
+        case "Helvetica":
+            $("#subtitulo").css("font-family", "Helvetica");
+            break;
+        case "Times":
+            //    $("#subtitulo").css("font-family", "Times, serif");
+            c.style.fontFamily = "Times, serif";
+            break;
+        case "Georgia":
+            $("#subtitulo").css("font-family", "Georgia");
+            break;
+    }
 }
 
-function mudarCor(x){
+function mudarCor(x) {
 
-    var valor= document.getElementById("cor").value;
-// var c2 = document.getElementById("subtitulo");
-var c = document.getElementById(x);
-c.style.color = valor;
+    var valor = document.getElementById("cor").value;
+    var c = document.getElementById(x);
+    c.style.color = valor;
 }
 
-function mudarTamanho(x){
-    var valor= document.getElementById("tamanho").value;
-    valor = parseInt(valor) +"px";
+function mudarTamanho(x) {
+    var valor = document.getElementById("tamanho").value;
+    valor = parseInt(valor) + "px";
     var c = document.getElementById(x);
     c.style.fontSize = valor;
-    // $("#subtitulo").css("font-size", valor); 
 }
 
 function verificaCampos() {
 
-var aChk = document.getElementsByName("item"); 
+    var aChk = document.getElementsByName("item");
 
-for (var i=0;i<aChk.length;i++){ 
+    for (var i = 0; i < aChk.length; i++) {
 
-    if (aChk[i].checked == true){ 
+        if (aChk[i].checked == true) {
 
-        var item = aChk[i].value;
-        mudarTipo(item);
-        mudarCor(item);
-        mudarTamanho(item);
-
-
-        // alert(aChk[i].value + " marcado.");
-
-        // CheckBox Não Marcado... Faça alguma outra coisa...
-
+            var item = aChk[i].value;
+            mudarTipo(item);
+            mudarCor(item);
+            mudarTamanho(item);
+        }
     }
-
 }
 
+function escolheMidia() {
+    // var mi = document.getElementByName("sec");
+    // mi.style.display = "none";
+
+    var aChk = document.getElementsByName("item");
+
+    for (var i = 0; i < aChk.length; i++) {
+
+        if (aChk[i].checked == true) {
+
+            var item = aChk[i].value;
+            if (item == "midia1") {
+                // var mi = document.getElementById("sec");
+                // mi.style.display = "block";
+                alert("Midia1");
+            }
+            if (item == "midia2") {
+                alert("Midia2");
+                // var mi = document.getElementsByName("sec");
+                mi.style.display = "none";
+            }
+            if (item == "midia3") {
+                alert("Midia3");
+            }
+        }
+    }
 }
-    
+
+
+
